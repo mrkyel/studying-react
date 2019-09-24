42a20dba0fd14ed1d32a317fe103bce7bc7180e4
mrkyel <kyel100@naver.com>
2019-09-24 15:33:28 +0900

    09-24

---
+++ src/Hello.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./Hello.css";
+
+class Hello extends Component {
+  state = { text: "" };
+
+  componentDidMount() {
+    let i = 0;
+
+    const typing = setInterval(() => {
+      const typed = this.state.text;
+      const finalText = "Web Developer 김한결입니다";
+
+      if (i < finalText.length) {
+        this.setState({
+          text: typed + finalText[i]
+        });
+        i++;
+      } else {
+        clearInterval(typing);
+      }
+    }, 178);
+  }
+
+  render() {
+    return (
+      <div className="myname" id="mrkyel">
+        <h1 className="myname-text">{this.state.text}</h1>
+      </div>
+    );
+  }
+}
+
+export default Hello;
